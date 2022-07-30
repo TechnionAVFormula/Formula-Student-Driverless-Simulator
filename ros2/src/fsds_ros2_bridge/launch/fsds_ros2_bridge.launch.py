afa3f472840a1189a6a7ab0469c26bb450d349d4
Alastair Bradford <albradford2468@gmail.com>
2022-07-30 18:45:56 +1000

    removed visualisation/tf duplicates from sim, now in qutms

--- ros2/src/fsds_ros2_bridge/launch/fsds_ros2_bridge.launch.py
+++ ros2/src/fsds_ros2_bridge/launch/fsds_ros2_bridge.launch.py
@@ -2,13 +2,10 @@ from os.path import expanduser, join
 import json 
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-from ament_index_python import get_package_share_directory
 
 CAMERA_FRAMERATE = 30.0
 
@@ -40,7 +37,6 @@ def generate_launch_description():
     track_arg = DeclareLaunchArgument(name='track_name', default_value='A')
     competition_arg = DeclareLaunchArgument(name='competition_mode', default_value='false')
     manual_arg = DeclareLaunchArgument(name='manual_mode', default_value='false')
-    rviz_arg = DeclareLaunchArgument(name='rviz', default_value='false')
 
     bridge_node = Node(
         package='fsds_ros2_bridge',
@@ -84,29 +80,14 @@ def generate_launch_description():
         ]
     )
 
-    tf_node = Node(
-        package='fsds_transforms',
-        executable='transform', 
-        output='screen',
-    )
-
-    rviz_pkg = get_package_share_directory('launch_rviz')
-    rviz_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([rviz_pkg, '/launch/display.launch.py']),
-        condition=IfCondition(LaunchConfiguration('rviz'))
-    )
-
     return LaunchDescription([
         host_arg,
         mission_arg,
         track_arg,
         competition_arg,
         manual_arg,
-        rviz_arg,
         bridge_node,
         *camera_nodes,
-        tf_node,
-        rviz_launch,
     ])
 
 
